af3144b1b69fdaf331aa8821110dfa42a1d10640
dengshiwei <dengshiwei>
2022-09-26 14:16:42 +0800

    init mpass_demo

---
+++ PortalDemoLauncher/app/src/main/java/com/antway/portal/launcher/MainActivity.java
@@ -0,0 +1,16 @@
+package com.antway.portal.launcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by mPaaS on 16/9/28.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.antway.portal.launcher.R.layout.main);
+    }
+}
